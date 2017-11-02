7eda0f0d899fd22341dba78d73c005c8aa762d79
hyper <hyperionos88@gmail.com>
2017-11-02 17:06:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-// write your code below!
+
 function happyHolidays() {
   return "Happy holidays!";
 }
-
-function happyHolidaysTo(holiday, name) {
-  return `Happy ${holiday}, ${name}!`;
-}
-
-function holidayCountdown(days, holiday) {
-  return `It\'s ${days} until ${"holiday"}`
-}
-
-var holiday = "Mother\'s Day";
-var days = 20;
